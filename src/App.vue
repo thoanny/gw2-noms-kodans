c6646ea751e61ed8083fa10327e245b8377066d4
Anthony Destenay <anthony.destenay@gmail.com>
2024-08-18 11:01:32 +0200

    First version of generator

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,52 @@
-<script setup></script>
+<script setup>
+import { ref, computed } from 'vue';
+
+import data from '@/data.json';
+
+const genders = ['féminin', 'masculin'];
+const gender = ref('');
+const fullname = ref(null);
+
+const setFullname = () => {
+    fullname.value = null;
+
+    const firstnames =
+        gender.value.length !== 0 ? data.firstname[gender.value].concat(data.firstname[2]) : null;
+
+    const lastnames =
+        gender.value.length !== 0 ? data.lastname[gender.value].concat(data.lastname[2]) : null;
+
+    if (firstnames && lastnames) {
+        const fid = Math.floor(Math.random() * firstnames.length);
+        const lid = Math.floor(Math.random() * lastnames.length);
+        const firstname = firstnames[fid];
+        const lastname = lastnames[lid];
+        fullname.value = `${firstname} ${lastname}`;
+    }
+};
+</script>
 
 <template>
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <div class="flex flex-col items-center justify-center text-center h-screen gap-6">
+        <div class="text-xl font-semibold">
+            Quel est ton nom kodan
+            <select name="gender" id="gender" v-model="gender" class="border" @change="setFullname">
+                <option value="">---</option>
+                <option v-for="(gender, g) in genders" :key="gender" :value="g">
+                    {{ gender }}
+                </option>
+            </select>
+            ?
+        </div>
+        <div class="text-2xl font-bold">{{ fullname }}</div>
+        <button v-if="gender !== ''" @click="setFullname" class="bg-gray-100 py-2 px-4">
+            Générer
+        </button>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+h2 {
+    @apply text-xl font-bold my-8;
+}
+</style>
